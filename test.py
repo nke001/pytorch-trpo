12aa089daee677f96dd66213a52519c6b8ef66be
Nan Rosemary Ke <rosemary.ke@west.cmu.edu>
2018-05-28 17:18:45 -0400

    save model properly

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from itertools import count
 
 import gym
 import scipy.optimize
-
+import pickle
 import torch
 from models import *
 from replay_memory import Memory
@@ -42,6 +42,8 @@ parser.add_argument('--render', action='store_true',
                     help='render the environment')
 parser.add_argument('--log-interval', type=int, default=1, metavar='N',
                     help='interval between training status logs (default: 10)')
+parser.add_argument('--write-data', type=bool, default=False, 
+                    help='whether to write the data (state, action) pairs to file')
 args = parser.parse_args()
 
 env = gym.make(args.env_name)
@@ -68,78 +70,27 @@ def select_action(state):
     action = torch.normal(action_mean, action_std)
     return action
 
-def update_params(batch):
-    rewards = torch.Tensor(batch.reward)
-    masks = torch.Tensor(batch.mask)
-    actions = torch.Tensor(np.concatenate(batch.action, 0))
-    states = torch.Tensor(batch.state)
-    values = value_net(Variable(states))
+def write_data(data, file_name):
+    with open(file_name, "wb") as fp:
+        pickle.dump(l, fp)
 
-    returns = torch.Tensor(actions.size(0),1)
-    deltas = torch.Tensor(actions.size(0),1)
-    advantages = torch.Tensor(actions.size(0),1)
-
-    prev_return = 0
-    prev_value = 0
-    prev_advantage = 0
-    for i in reversed(range(rewards.size(0))):
-        returns[i] = rewards[i] + args.gamma * prev_return * masks[i]
-        deltas[i] = rewards[i] + args.gamma * prev_value * masks[i] - values.data[i]
-        advantages[i] = deltas[i] + args.gamma * args.tau * prev_advantage * masks[i]
-
-        prev_return = returns[i, 0]
-        prev_value = values.data[i, 0]
-        prev_advantage = advantages[i, 0]
-
-    targets = Variable(returns)
-
-    # Original code uses the same LBFGS to optimize the value loss
-    def get_value_loss(flat_params):
-        set_flat_params_to(value_net, torch.Tensor(flat_params))
-        for param in value_net.parameters():
-            if param.grad is not None:
-                param.grad.data.fill_(0)
-
-        values_ = value_net(Variable(states))
-
-        value_loss = (values_ - targets).pow(2).mean()
-
-        # weight decay
-        for param in value_net.parameters():
-            value_loss += param.pow(2).sum() * args.l2_reg
-        value_loss.backward()
-        return (value_loss.data.double().numpy()[()], get_flat_grad_from(value_net).data.double().numpy())
-
-    flat_params, _, opt_info = scipy.optimize.fmin_l_bfgs_b(get_value_loss, get_flat_params_from(value_net).double().numpy(), maxiter=25)
-    set_flat_params_to(value_net, torch.Tensor(flat_params))
-
-    advantages = (advantages - advantages.mean()) / advantages.std()
-
-    action_means, action_log_stds, action_stds = policy_net(Variable(states))
-    fixed_log_prob = normal_log_density(Variable(actions), action_means, action_log_stds, action_stds).data.clone()
-
-    def get_loss(volatile=False):
-        action_means, action_log_stds, action_stds = policy_net(Variable(states, volatile=volatile))
-        log_prob = normal_log_density(Variable(actions), action_means, action_log_stds, action_stds)
-        action_loss = -Variable(advantages) * torch.exp(log_prob - Variable(fixed_log_prob))
-        return action_loss.mean()
-
-
-    def get_kl():
-        mean1, log_std1, std1 = policy_net(Variable(states))
-
-        mean0 = Variable(mean1.data)
-        log_std0 = Variable(log_std1.data)
-        std0 = Variable(std1.data)
-        kl = log_std1 - log_std0 + (std0.pow(2) + (mean0 - mean1).pow(2)) / (2.0 * std1.pow(2)) - 0.5
-        return kl.sum(1, keepdim=True)
-
-    trpo_step(policy_net, get_loss, get_kl, args.max_kl, args.damping)
+def load_data(data, file_name):
+    with open(file_name, "rb") as fp:   
+        b = pickle.load(fp)
 
 running_state = ZFilter((num_inputs,), clip=5)
 running_reward = ZFilter((1,), demean=False, clip=10)
 
+
+reload = True
+if reload:
+    policy_net = load_param(policy_net, 'Reacher_policy_copy.pkl')
+    value_net = load_param(value_net, 'Reacher_value_copy.pkl')
+
+# data is the list of state_action pairs
+data = []
 for i_episode in count(1):
+    episode = []
     memory = Memory()
 
     num_steps = 0
@@ -155,14 +106,14 @@ for i_episode in count(1):
             action = action.data[0].numpy()
             next_state, reward, done, _ = env.step(action)
             reward_sum += reward
-
             next_state = running_state(next_state)
 
             mask = 1
             if done:
                 mask = 0
-
+            import ipdb; ipdb.set_trace()
             memory.push(state, np.array([action]), mask, next_state, reward)
+            #save_param(policy_net, 'Reacher.pkl')
             if args.render:
                 env.render()
             if done:
@@ -175,10 +126,7 @@ for i_episode in count(1):
 
     reward_batch /= num_episodes
     batch = memory.sample()
-    update_params(batch)
-    
-    save_param(policy_net, 'Reacher_policy.pkl')
-    save_param(value_net, 'Reacher_value.pkl')
+
     if i_episode % args.log_interval == 0:
         print('Episode {}\tLast reward: {}\tAverage reward {:.2f}'.format(
             i_episode, reward_sum, reward_batch))
