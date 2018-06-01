03bf17cca1d6cd0b82d8afeaa9d9d4db9df583a9
Nan Rosemary Ke <rosemary.ke@west.cmu.edu>
2018-06-01 02:22:36 -0400

    good hyper parameters

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 # good set of hyperparameters
 
-python zforcing_main.py --batch-size 10  --aux-weight-start 0.01 --kld-weight-start 0.1 --bwd-weight 1.0  --lr 1e-5 --aux-weight-end 0.001
-
+python zforcing_main.py --batch-size 10  --aux-weight-start 0.1 --kld-weight-start 0.1 --bwd-weight 1.0  --lr 1e-3 --aux-weight-end 0.001
